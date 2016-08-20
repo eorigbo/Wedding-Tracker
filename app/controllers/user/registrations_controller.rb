92a9396bd549ad7ae7fcf4aff2ae52b1d55cc487
eorigbo <eorigbo@gmail.com>
2016-08-20 16:31:20 +0000

    modified edit user page and navbar logo styling

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,6 +1,8 @@
 class User::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
   
+  
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
