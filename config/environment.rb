6132e08023dda2f6a7e128803025bd060a386fae
eorigbo <eorigbo@gmail.com>
2016-10-08 16:55:23 +0000

    updated email settings

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,5 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
   :enable_starttls_auto =>  true
 }
