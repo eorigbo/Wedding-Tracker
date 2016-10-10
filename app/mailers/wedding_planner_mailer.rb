f49eafc0b018b500afb259ab25d0690f8886cf3c
eorigbo <eorigbo@gmail.com>
2016-10-10 03:24:34 +0000

    modified subject line of email

---
+++ app/mailers/wedding_planner_mailer.rb
@@ -0,0 +1,16 @@
+class WeddingPlannerMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that you mailer uses the devise views
+
+  def confirmation_instructions(record, token, opts={})
+    if @resource.first_name.present?
+      opts[:subject] = "Welcome #{@resource.first_name}, please confirm your email address"
+    else
+      opts[:subject] = "Please confirm your email address"
+    end
+
+    super
+  end
+
+end
