20c5a0047f0f84f25e0535069367c28f30739be6
eorigbo <eorigbo@gmail.com>
2016-11-13 17:47:39 +0000

    fixing flash message bugs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
   end
+  
+  def testalert
+    #flash[:success] = 'Just testing the alert.'
+  end
 end
